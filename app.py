ec7a0b904a9bb524c9d9a73801b69f01701d36e6
Claudiu Claudiu <office.claudiuclement@gmail.com>
2023-04-25 20:30:24 +0200

    Updated Flask app to use Heroku-assigned port

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def calculate():
         return jsonify({'error': "Could not find tilting point within reasonable limits."})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
